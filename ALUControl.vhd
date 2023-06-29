869cde2aef6dd41ef3b5f6475f23e8bd8a7aa7e0
mesmi <71196600+mesmi@users.noreply.github.com>
2023-06-29 10:52:51 -0400

    Update ALUControl.vhd

--- ALUControl.vhd
+++ ALUControl.vhd
@@ -12,5 +12,5 @@ ARCHITECTURE rtl OF ALUControl IS
 BEGIN
 	Op(2)<= ALUop(0) OR (funct(1)AND ALUop(1));
 	Op(1)<= NOT(funct(2)) OR (NOT ALUop(1));
-	Op(0)<= ALUop(1) AND (funct(3)OR funct(0));
+	Op(0)<= ALUop(1) AND (funct(0)OR funct(3));
 END rtl;
